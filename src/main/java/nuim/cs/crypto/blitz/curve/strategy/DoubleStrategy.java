e607aca8cb8c84b9757b2351f189aaa5f9dcf466
Hellblazer <hellblazer@me.com>
2013-12-10 11:46:53 -0800

    First attempt at subverting JCE signing for testing and development

--- src/main/java/nuim/cs/crypto/blitz/curve/strategy/DoubleStrategy.java
+++
@@ -1,7 +0,0 @@
-package nuim.cs.crypto.blitz.curve.strategy;
-
-import nuim.cs.crypto.blitz.point.AffinePoint;
-
-public interface DoubleStrategy {
-    public AffinePoint doublePoint(AffinePoint p);
-}
--- src/main/java/nuim/cs/crypto/blitz/point/Point.java
+++
@@ -1,9 +0,0 @@
-package nuim.cs.crypto.blitz.point;
-
-import nuim.cs.crypto.blitz.field.Element;
-
-public interface Point {
-    Element x();
-
-    Element y();
-}
